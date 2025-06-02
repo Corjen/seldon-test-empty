56e5dc4dffd1c8a22192079d0472fa582e0be228
corjen <corjen@reverseddigital.com>
2025-06-02 13:42:26 +0200

    Update Title.tsx

---
+++ components/seldon/primitives/Title.tsx
@@ -0,0 +1,88 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { HTMLDiv } from "../native-react/HTML.Div";
+import { HTMLHeading1 } from "../native-react/HTML.Heading1";
+import { HTMLHeading2 } from "../native-react/HTML.Heading2";
+import { HTMLHeading3 } from "../native-react/HTML.Heading3";
+import { HTMLHeading4 } from "../native-react/HTML.Heading4";
+import { HTMLHeading5 } from "../native-react/HTML.Heading5";
+import { HTMLHeading6 } from "../native-react/HTML.Heading6";
+import { CSSProperties } from "react";
+import { HTMLAttributes } from "react";
+
+interface TitleTokens {
+  htmlElement?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+}
+
+type TitleProps = HTMLAttributes<HTMLHeadingElement> & TitleTokens;
+
+export const Title = ({ htmlElement, style, ...props }: TitleProps) => {
+  const styles = style || defaultStyles;
+
+  switch (htmlElement) {
+    case "h1":
+      return (
+        <HTMLHeading1 style={styles} {...props}>
+          {props.children}
+        </HTMLHeading1>
+      );
+    case "h2":
+      return (
+        <HTMLHeading2 style={styles} {...props}>
+          {props.children}
+        </HTMLHeading2>
+      );
+    case "h3":
+      return (
+        <HTMLHeading3 style={styles} {...props}>
+          {props.children}
+        </HTMLHeading3>
+      );
+    case "h4":
+      return (
+        <HTMLHeading4 style={styles} {...props}>
+          {props.children}
+        </HTMLHeading4>
+      );
+    case "h5":
+      return (
+        <HTMLHeading5 style={styles} {...props}>
+          {props.children}
+        </HTMLHeading5>
+      );
+    case "h6":
+      return (
+        <HTMLHeading6 style={styles} {...props}>
+          {props.children}
+        </HTMLHeading6>
+      );
+    default:
+      return (
+        <HTMLDiv style={styles} {...props}>
+          {props.children}
+        </HTMLDiv>
+      );
+  }
+};
+
+const defaultStyles: CSSProperties = {
+  color: "hsl(0deg 4% 8%)",
+  alignSelf: "stretch",
+  height: "fit-content",
+  fontFamily: "Inter",
+  fontStyle: "normal",
+  fontSynthesisStyle: "none",
+  fontWeight: 500,
+  fontSize: "1rem",
+  lineHeight: 1.25,
+  whiteSpace: "normal",
+  overflow: "hidden",
+  display: "-webkit-box",
+  WebkitLineClamp: 2,
+  lineClamp: 2,
+  WebkitBoxOrient: "vertical",
+  boxOrient: "vertical",
+};
